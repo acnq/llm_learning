92dc4e0c3cd6f9f6a5173855bf3039830e3f3e21
acnq <fanyuanhaotc@163.com>
2025-09-11 00:33:12 +0800

    每日一题

---
+++ 01_llm_using/1.prompts/tool.py
@@ -0,0 +1,33 @@
+import os
+
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
+
+def get_openai_key():
+  # 读取本地/项目的环境变量。
+
+  # find_dotenv()寻找并定位.env文件的路径
+  # load_dotenv()读取该.env文件，并将其中的环境变量加载到当前的运行环境中  
+  # 如果你设置的是全局的环境变量，这行代码则没有任何作用。
+  _ = load_dotenv(find_dotenv())
+  return os.environ['OPENAI_API_KEY']
+
+
+client = OpenAI(api_key=get_openai_key(), base_url="https://api.deepseek.com/beta")
+
+def get_completion(prompt, model="deepseek-chat"):
+  '''
+  prompt: 对应的提示词
+  model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)，有内测资格的用户可以选择 gpt-4
+  '''
+  messages = [{"role": "user", "content": prompt}]
+  response = client.chat.completions.create(
+    model=model,
+    messages=messages,
+    temperature=0,
+  )
+  # 调用OpenAI的ChatCompletion 接口
+  return response.choices[0].message.content
+
+  
+  
--- 01_llm_using/README.md
+++ 01_llm_using/README.md
@@ -1,4 +1,5 @@
 ## A Project Learning Basic Concepts and Usage of LLM
 
 imitated here: https://github.com/datawhalechina/llm-cookbook 
+
 instructed here: https://www.bilibili.com/video/BV1Bo4y1A7FU/ 
